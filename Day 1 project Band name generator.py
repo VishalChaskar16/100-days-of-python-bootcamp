0f804783f64e481cd8c2d2f98a3ac7c3078c4de3
Vishal Chaskar <149327429+VishalChaskar16@users.noreply.github.com>
2025-09-12 21:17:41 +0530

    Add files via upload

---
+++ Day 1 project Band name generator.py
@@ -0,0 +1,16 @@
+print("Welcome to my band name generator!") 
+# The print function prints the Welcome message
+
+print("Hello " + input("What is your name?\n "))
+#The print function prints a Hello message along with the name of the User by using the input functiom
+#The '\n' method helps to print the input message on the next line
+
+city_name = input("What city you grew up in?\n ")
+#The city_name is the name of the varibale that holds the input function
+
+pet_name = input("What is the name of your pet?\n ")
+#The pet_name is the name of the variable that holds the next input function
+
+print("Your band name could be " + city_name + " " + pet_name)
+#The print function prints the Band name message by calling the variable names 
+# + method is used for concatenation of variable names & the double quotes is used to apply space between the generated name
